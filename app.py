7b3c65553560ed893a76094e8a733eb1e2d9f624
VaniaGalimzianov <144653733+VaniaGalimzianov@users.noreply.github.com>
2025-02-18 17:17:19 +0300

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ os.makedirs(BASE_DIR, exist_ok=True)
 
 def fetch_protein_data(protein_id):
     fasta_url = f"https://rest.uniprot.org/uniprotkb/{protein_id}.fasta"
-    json_url = f"https://rest.uniprot.org/uniprotkb/{protein_id}.json?fields=ft_helix,ft_strand,ft_turn"
+    json_url = f"https://rest.uniprot.org/uniprotkb/{protein_id}.json?fields=ft_var_seq,ft_variant,ft_non_cons,ft_non_std,ft_non_ter,ft_conflict,ft_unsure,ft_act_site,ft_binding,ft_dna_bind,ft_site,ft_mutagen,ft_intramem,ft_topo_dom,ft_transmem,ft_chain,ft_crosslnk,ft_disulfid,ft_carbohyd,ft_init_met,ft_lipid,ft_mod_res,ft_peptide,ft_propep,ft_signal,ft_transit,ft_strand,ft_helix,ft_turn,ft_coiled,ft_compbias,ft_domain,ft_motif,ft_region,ft_repeat,ft_zn_fing"
 
     fasta_response = requests.get(fasta_url)
     json_response = requests.get(json_url)
